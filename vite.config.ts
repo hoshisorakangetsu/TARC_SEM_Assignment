c184b3bff099e9a655a4a5ae530c9853f8ed38f4
hanyue1014 <wongxuanbin@gmail.com>
2024-09-16 22:27:52 +0800

    Add JPG as detected asets

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  assetsInclude: ['**/*.jpg'],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
