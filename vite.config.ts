5cce39a99803037f9e0c2e64a79b3c3da107c969
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2024-09-18 09:23:31 +0800

    Add asset include

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
-  assetsInclude: ['**/*.jpg'],
+  assetsInclude: ["**/*.jpg", "**/*.JPG"],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
