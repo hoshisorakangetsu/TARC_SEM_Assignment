6018b9cda05bfd0ba424d83e2182a5f4f05b2002
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2024-09-19 12:30:17 +0800

    Add mock data type

--- src/data.ts
+++ src/data.ts
@@ -1,27 +1,90 @@
 export interface Course {
   title: string;
+  overview: string;
+  campus: string[];
+  intake: string;
+  duration: string;
+  entryRequirements: string[];
+  outline: string[];
+  progression: string[];
+  career: string[];
+  fee: string;
 }
 
 export const DiplomaCourses: Course[] = [
   {
     title: "Diploma in Computer Science",
+    overview: "",
+    campus: [],
+    intake: "",
+    duration: "",
+    entryRequirements: [],
+    outline: [],
+    progression: [],
+    career: [],
+    fee: "",
   },
   {
     title: "Diploma in Information Technology",
+    overview: "",
+    campus: [],
+    intake: "",
+    duration: "",
+    entryRequirements: [],
+    outline: [],
+    progression: [],
+    career: [],
+    fee: "",
   },
   {
     title: "Diploma in Information Technology",
+    overview: "",
+    campus: [],
+    intake: "",
+    duration: "",
+    entryRequirements: [],
+    outline: [],
+    progression: [],
+    career: [],
+    fee: "",
   },
 ];
 
 export const DegreeCourses: Course[] = [
   {
     title: "Bachelor Degree in Computer Science",
+    overview: "",
+    campus: [],
+    intake: "",
+    duration: "",
+    entryRequirements: [],
+    outline: [],
+    progression: [],
+    career: [],
+    fee: "",
   },
   {
     title: "Bachelor Degree in Information Technology",
+    overview: "",
+    campus: [],
+    intake: "",
+    duration: "",
+    entryRequirements: [],
+    outline: [],
+    progression: [],
+    career: [],
+    fee: "",
   },
   {
     title: "Bachelor Degree in Information Technology",
+    overview: "",
+    campus: [],
+    intake: "",
+    duration: "",
+    entryRequirements: [],
+    outline: [],
+    progression: [],
+    career: [],
+    fee: "",
   },
 ];
