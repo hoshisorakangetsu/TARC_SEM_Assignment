9ecf192f903c3301bd277da2a12a9e67512c8fe0
hanyue1014 <wongxuanbin@gmail.com>
2024-09-19 23:14:02 +0800

    Add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
-  base: "/TARC_SEM_Assignment",
+  base: "/TARC_SEM_Assignment/",
   assetsInclude: ["**/*.jpg", "**/*.JPG"],
   resolve: {
     alias: {
