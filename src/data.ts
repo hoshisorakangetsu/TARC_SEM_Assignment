64a5f9dd3fb4caf7b2a518b9ddbe393c5474a18b
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2024-09-19 12:24:56 +0800

    Add mock data

--- src/data.ts
+++ src/data.ts
@@ -2,4 +2,26 @@ export interface Course {
   title: string;
 }
 
-export const courses: { [id: number]: Course } = {};
+export const DiplomaCourses: Course[] = [
+  {
+    title: "Diploma in Computer Science",
+  },
+  {
+    title: "Diploma in Information Technology",
+  },
+  {
+    title: "Diploma in Information Technology",
+  },
+];
+
+export const DegreeCourses: Course[] = [
+  {
+    title: "Bachelor Degree in Computer Science",
+  },
+  {
+    title: "Bachelor Degree in Information Technology",
+  },
+  {
+    title: "Bachelor Degree in Information Technology",
+  },
+];
